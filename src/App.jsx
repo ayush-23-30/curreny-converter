6b39bed4652b5b51f439de9835d4b5a060bcb009
ayush-23-30 <pawar85060@gmail.com>
2024-09-13 20:30:54 +0530

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CurrencyConvertor from "./components/CurrencyConvertor"
+
+function App() {
+
+
+  return (
+    <div className="font-bold min-h-screen flex flex-col items-center justify-center  bg-gray-100 ">
+   <CurrencyConvertor/>
+    </div>
+     
+  )
+}
+
+export default App
